c1dd1696943b44073b2e48346a6d0ea91cd04203
Gustavo Santos <gustavosantos@pop-os.localdomain>
2021-07-19 20:08:10 -0300

    Add expo fonts to project

--- App.tsx
+++ App.tsx
@@ -1,13 +1,33 @@
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components';
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins';
 
 import { Welcome } from './src/screens/Welcome';
 
 import theme from './src/global/styles/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar style="auto" />
       <Welcome />
     </ThemeProvider>
   );
