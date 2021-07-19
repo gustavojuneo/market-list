e2f4da7352ae0b378d46b4dc0c8772a6530f7da7
Gustavo Santos <gustavosantos@pop-os.localdomain>
2021-07-19 20:02:00 -0300

    Initial configurations

---
+++ src/screens/Welcome/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container, Title } from './styles';
+
+export function Welcome() {
+  return (
+    <Container>
+      <Title>Welcome to application.</Title>
+    </Container>
+  );
+}
